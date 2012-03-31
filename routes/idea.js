91298822f5b555ac1acd0184291ff90ba1106dba
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-31 19:55:53 -0400

    Fixed idea model create

--- routes/idea.js
+++ routes/idea.js
@@ -1,4 +1,5 @@
 var Idea = require("../models/ideaModel.js"),
+	User = require("../models/userModel.js"),
 	mongoose = require('mongoose'),
 	ObjectId = mongoose.Types.ObjectId;
 
@@ -6,19 +7,71 @@ module.exports = {
 	
 	create :
 		function(req, res){
-			var newIdea = new Idea({
-				'idea': req.body.idea,
-				'prev': new ObjectId(req.body.prev),
-				'owner': new ObjectId(req.body.owner),
-				'name': req.body.name,
-				'timestamp': Date.now()
-			});
-			newIdea.save();
-			var response = {
-				status: "Success",
-				data: "Just created a idea"
-			};
-			res.send( response );
+			var response;
+			var newIdea;
+			if(req.body.owner) {
+				User.findOne({_id: new ObjectId(req.body.owner)}, function(err, doc) {
+					if(err) {
+						response = {
+							status: "Error",
+							data: err
+						};
+						res.send(response);
+					} else if(doc !== null) {
+						if(req.body.prev) {
+							User.findOne({_id: new ObjectId(req.body.prev)}, function(err, doc2) {
+								if(err) {
+									response = {
+										status: "Error",
+										data: err
+									};
+								} else if(doc !== null) {
+									newIdea = new Idea({
+										'idea': req.body.idea,
+										'prev': doc2,
+										'owner': doc,
+										'name': req.body.name,
+										'timestamp': Date.now()
+									});
+									newIdea.markModified('idea');
+									newIdea.save();
+									response = {
+										status: "Success",
+										data: newIdea._id
+									};
+								}
+								res.send(response);
+							});
+						} else {
+							newIdea = new Idea({
+								'idea': req.body.idea,
+								'owner': doc,
+								'name': req.body.name,
+								'timestamp': Date.now()
+							});
+							newIdea.markModified('idea');
+							newIdea.save();
+							response = {
+								status: "Success",
+								data: newIdea._id
+							};
+							res.send(response);
+						}
+					} else {
+						response = {
+							status: "Failure",
+							data: "No owner by this Id could be found"
+						};
+						res.send(response);
+					}
+				});
+			} else {
+				response = {
+					status: "Failure",
+					data: "You didnt give me an owner Id!"
+				};
+				res.send(response);
+			}
 		},
 	
 	
