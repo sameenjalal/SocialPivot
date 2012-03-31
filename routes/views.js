934107a19184ea566fe8e64ba5aaabccc7e609cc
Devon Peticolas <dpeticol@eden.rutgers.edu>
2012-03-31 15:18:45 -0400

    sorted recents for profile view, idea view done

--- routes/views.js
+++ routes/views.js
@@ -1,6 +1,8 @@
+/* requires */
 User = require('../models/userModel.js');
 Idea = require('../models/ideaModel.js');
 Comment = require('../models/commentModel.js');
+
 
 function internalError(res, err){
 	console.log('Error : '+err);
@@ -8,14 +10,49 @@ function internalError(res, err){
 	res.end('Internal Server Error!');
 }
 
+function sortChrono(event1, event2){
+	if(event1.date < event2.date){
+		return 1;
+	else if(event1.date > event2.date){
+		return -1;
+	else{
+		return 0;
+	}
+}
+
+/* exported functions */
 module.exports = {
 
 	/* renders the idea view for a :ideaID that corrosponds to db ID */
 	ideaView :
 		function(req, res){
 			
+			/* find the idea */
 			Idea.findById(req.params.ideaID, function(err, idea){
-				res.render(idea, {idea : idea});
+				if(err){
+					internalServerError(res, err);
+				}else if(idea === null){
+					res.writeHead(404);
+					res.end('Could not find Idea');
+				}else{
+					
+					/* find the comments */
+					Comment.find({
+						idea : req.params.ideaID
+					}, function(err, comments){
+						if(err){
+							internalServerError(res, err);
+						}else{
+							comments.sort(sortChrono);
+							
+							/* render the view */
+							res.render(idea, {
+								idea : idea, 
+								comments : comments
+							});
+						}
+					});
+				}
 			});
 		},
 
@@ -24,17 +61,21 @@ module.exports = {
 	searchView :
 		function(req, res){
 			
-			 
+						 
 		},
 
 
+	/* renders the feed view for most recent activity */
+	feedView :
+		function(req, res){
 
+		},
 
 
 	/* renders the profile for a :username passed as a param */
 	profileView : 
 		function(req, res){
-		
+			
 			/* get user data */
 			User.findOne({
 				name : req.params.username
@@ -62,11 +103,18 @@ module.exports = {
 									internalError(res, err);
 								}else{
 									
+									/* sort the top 10 recent activity */
+									var activity = [];
+									activity.push(ideas);
+									activity.push(comments);
+									activity.sort(sortChrono);
+
 									/* render user profile */
 									res.render('profileView.ejs', {
-										user : foundUser,
-										ideas : foundIdeas,
-										comments : foundComments
+										user : foundUsers.sort(sortChrono),
+										ideas : foundIdeas.sort(sortChrono),
+										comments : foundComments.sort(sortChrono),
+										recent : activity.splice(0, 10)
 									});
 								}
 							});
@@ -74,46 +122,6 @@ module.exports = {
 					});
 				}
 			});
-		},
-							
+		}
 
-
-		/* get user data */
-		User.findOne({
-			name : req.params.username
-		}, function(err, foundUser){
-			if(err){
-				internalError(res, err);
-			}else if(foundUser === null){
-				res.writeHead(404);
-				res.end('Unknown User');
-			}else{
-
-				/* get recipies made by user */
-				Recipie.find({
-					owner: foundUser._id
-				}, function(err, foundRecipes){
-					if(err){
-						internalError(res, err);
-					}else{
-
-						/* get reviews written by user */
-						Review.find({
-							owner: foundUser._id
-						}, function(err, foundReviews){
-							if(err){
-								internalError(res, err);
-							}else{
-								res.render('profileView.ejs', {
-									user : foundUser,
-									recipies : foundRecipes,
-									reviews : foundReviews
-								});
-							}
-						});
-					}
-				});
-			}
-		});
-	}
 };
