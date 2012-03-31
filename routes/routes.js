6feafb6b29b4c23d0e8b3ba88a8559c4fb6e8b90
Devon Peticolas <dpeticol@eden.rutgers.edu>
2012-03-30 23:08:41 -0400

    basic server, hello world is up and running

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+module.exports = {
+	
+	helloworld :
+		function(req, res){
+			res.send("hello world");
+		}
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+/* enviroment */
+var port = process.env.PORT || 4242
+
+/* requires */
+var express = require("express");
+var routes = require("./routes/routes.js");
+
+
+/* create server */
+var app = express.createServer();
+
+/* configs */
+app.configure(function(){
+	app.use(express.static(__dirname+"/public"));
+	app.set('views', __dirname+'/views');
+	app.set('view engine', 'ejs');
+	app.set('view options', {
+		layout : false
+	});
+});
+
+
+/* CRUD */
+app.get('/', routes.helloworld);
+
+
+/* start 'er up */
+app.listen(port);
+console.log("server has started on port "+port);
