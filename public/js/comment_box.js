5282da082ab3c486f03334a9d15df9e01b79a1e8
Sameen Jalal <sameenjalal@gmail.com>
2012-04-01 06:45:25 -0700

    asdf

--- public/js/comment_box.js
+++ public/js/comment_box.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	alert("asdf");
 	var comment_div = $('#comment_div');
 	var tog_button = $('#comment_box_button');
 	var open = false;
@@ -8,11 +9,9 @@ $(function() {
 	comment_div.hide();
 	tog_button.on("click", function() {
 		if( open === false ) {
-			tog_button.attr( "value", "Show Comment Box" );
 			comment_div.slideToggle();
 			open = true;
 		} else {
-			tog_button.attr( "value", "Hide Comment Box" );
 			comment_div.slideToggle();
 			open = false;
 		}
