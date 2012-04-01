6f5924befad702c694df5c0a11366a669d0e2f0b
Andrew Jaeger <mrjaeger00@gmail.com>
2012-04-01 09:44:41 -0400

    STAY FIXED

--- routes/views.js
+++ routes/views.js
@@ -87,7 +87,7 @@ module.exports = {
 	saveIdea:
 		function(req, res) {
 			var post_data = "";
-			req.body.owner = req.session.user.id;
+			req.body.owner = req.session.user._id;
 			for( var key in req.body) {
 				if(req.body.hasOwnProperty(key)) {
 					post_data += key + "=" + req.body[key] + "&";
