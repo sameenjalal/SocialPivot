c1ba881a2aedcebee4b9a0bf9b7f7d82c2425950
Devon Peticolas <dpeticol@eden.rutgers.edu>
2012-03-31 20:55:21 -0400

    changed schemas to a single file

--- models/ideaModel.js
+++ models/ideaModel.js
@@ -1,15 +1,4 @@
 var mongoose = require('mongoose');
-var ObjectId = mongoose.Schema.ObjectId;
-var User = require('./userModel.js');
+var schemas = require('./schemas.js');
 
-var Schema = mongoose.Schema;
-
-var ideaSchema = new Schema({
-	'idea': {},
-	'prev': User,
-	'owner': User,
-	'name': String,
-	'timestamp': Date
-});
-
-module.exports = mongoose.model('Idea', ideaSchema);
+module.exports = mongoose.model('Idea', schemas.idea);
